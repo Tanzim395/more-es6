a834715b5667a5a3ec697136b1d87bc9b7f7604c
Tanzim395 <junaidulkader@gmail.com>
2021-08-25 17:19:41 +0600

    arr dest

--- try.js
+++ try.js
@@ -3,6 +3,11 @@
 
 // const { x, y, z } = { x: 1, y1: 2, z: 3 };
 
-const nums = [1, 2, 3, 4, 5];
-let output = nums.filter(n => n % 2);
-console.log(output);
+// const nums = [1, 2, 3, 4, 5];
+// let output = nums.filter(n => n % 2);
+// console.log(output);
+
+const array = [1, 2, 3, 4, 4]
+const [, , number3] = array;
+const third = number3
+console.log(third);
